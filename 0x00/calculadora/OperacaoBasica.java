bf3c5739d55496417261ed1c28d25b67cc1e5344
Flávia Almeida <eufga1@gmail.com>
2022-03-04 14:00:02 -0300

    Add files via upload

---
+++ 0x00/calculadora/OperacaoBasica.java
@@ -0,0 +1,15 @@
+public class OperacaoBasica {
+	
+	public static double somar(double valor1, double valor2) {
+		return valor1+valor2;
+	}
+	public static double subtrair(double valor1, double valor2) {
+		return valor1-valor2;
+	}
+	public static double multiplicar(double valor1, double valor2) {
+		return valor1*valor2;
+	}
+	public static double dividir(double valor1, double valor2) {
+		return valor1/valor2;
+	}
+}
