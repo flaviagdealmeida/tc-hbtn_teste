0f8ca8e4c350cc73f65dc6893945de5ecf17a659
Flávia Almeida <eufga1@gmail.com>
2022-03-04 13:54:05 -0300

    Add files via upload

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,13 @@
+package email;
+
+public class Pessoa {
+	
+	private String email;
+	
+	
+	public static boolean emailValid(String email) {
+			return (email.length() <= 50 ||  email.contains("@"))? true: false;
+
+	}
+
+}
---
+++ 0x00/email/Program.java
@@ -0,0 +1,18 @@
+package email;
+
+public class Program {
+	public static void main(String[] args) {
+
+		System.out.println("Teste de email válido");
+		boolean isValidEmail = TesteEmail.testar_email_com_arroba();
+		System.out.println(isValidEmail);
+
+		System.out.println("Teste de email sem @");
+		isValidEmail = TesteEmail.testar_email_sem_arroba();
+		System.out.println(isValidEmail);
+
+		System.out.println("Teste de email com mais de 50 caracteres");
+		isValidEmail = TesteEmail.testar_email_mais_50_caracteres();
+		System.out.println(isValidEmail);
+	}
+}
